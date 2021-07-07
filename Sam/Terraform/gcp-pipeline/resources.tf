7db392f2c43319a5aa2df49a569e9398cf42cde9
Sam McKeating <e-smmg@gft.com>
2021-07-07 13:35:26 +0100

    Adds terraform cloud bucket creation * Adds file.txt to be uploaded to bucket * Specifies bucket in resources

--- Sam/Terraform/gcp-pipeline/resources.tf
+++ Sam/Terraform/gcp-pipeline/resources.tf
@@ -2,3 +2,13 @@
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/storage_bucket
 # https://www.terraform.io/docs/language/data-sources/index.html
 
+resource "google_storage_bucket" "store-file" {
+  name   = "store-file"
+  location = "EU"
+}
+
+resource "google_storage_bucket_object" "file" {
+  name   = "file01"
+  source = "file.txt"
+  bucket = "store-file"
+}
