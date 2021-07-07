b844df68a041fc55bdb782e20f8d43f3c9619927
Dhruv Parekh <e-dvph@GFT.com>
2021-07-07 14:28:11 +0100

    Made changes to resources.tf

--- Dhruv/terraform/resources.tf
+++ Dhruv/terraform/resources.tf
@@ -2,13 +2,13 @@
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/storage_account
 # https://www.terraform.io/docs/language/data-sources/index.html
 
- resource "random_integer" "suffix" {
-    min = 1
-    max = 99999
+resource "random_integer" "priority" {
+  min = 1
+  max = 50000
 }
 
-resource "azurerm_storage_account" "DhruvaccountTerra" {
-  name                     = "Dhruv${random_integer.suffix.result}"
+resource "azurerm_storage_account" "lana_storage_account" {
+  name                     = "lana${random_integer.priority.result}"
   resource_group_name      = data.azurerm_resource_group.padawans.name
   location                 = data.azurerm_resource_group.padawans.location
   account_tier             = "Standard"
