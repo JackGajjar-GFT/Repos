d73f60d3a5c7f8bdfdb346d86fbca51d37da0e9e
Pasang <pasangsolti@hotmail.co.uk>
2021-07-07 14:42:10 +0100

    bonus tasks

--- Pasang/terraform/resources.tf
+++ Pasang/terraform/resources.tf
@@ -2,3 +2,20 @@
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/storage_bucket
 # https://www.terraform.io/docs/language/data-sources/index.html
 
+resource "random_integer" "suffix" {
+    min=1
+    mix=99999
+}
+data "google_project" "project" {
+}
+resource "google_storage_bucket" "storage" {
+  name          = "Pasang${random_integer.suffix.result}"
+  location      = "US"
+
+}
+
+
+
+
+
+ 
