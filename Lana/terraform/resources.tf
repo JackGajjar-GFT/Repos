86a8cecf8a0836c20905f25d9c03aba7f829ebc2
Lana Arif <e-laaf@gft.com>
2021-07-07 13:19:52 +0100

    deploys an azure storage account through terraform

--- Lana/terraform/resources.tf
+++ Lana/terraform/resources.tf
@@ -2,3 +2,19 @@
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/storage_account
 # https://www.terraform.io/docs/language/data-sources/index.html
 
+resource "random_integer" "priority" {
+  min = 1
+  max = 50000
+}
+
+resource "azurerm_storage_account" "lana_storage_account" {
+  name                     = "lana${random_integer.priority.result}"
+  resource_group_name      = data.azurerm_resource_group.padawans.name
+  location                 = data.azurerm_resource_group.padawans.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
