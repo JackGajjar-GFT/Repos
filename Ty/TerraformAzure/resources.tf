8b97cacc4be9ece2ea843e76589b46843a73e1f9
Tyrese Henry <e-tehy@gft.com>
2021-07-07 14:28:15 +0100

    update resources to create storage account

--- Ty/TerraformAzure/resources.tf
+++ Ty/TerraformAzure/resources.tf
@@ -2,3 +2,19 @@
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/storage_account
 # https://www.terraform.io/docs/language/data-sources/index.html
 
+resource "azurerm_resource_group" "example" {
+  name     = "padawans-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "storageaccountname"
+  resource_group_name      = azurerm_resource_group.padawans.name
+  location                 = azurerm_resource_group.padawans.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
