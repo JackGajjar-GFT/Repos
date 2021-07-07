c63749b28f746321cd2f7b7bffb45857891f1a00
Benjamin Hill <benjamin.hill@gft.com>
2021-07-07 10:46:58 +0100

    added storage account

--- Ben/terraform/resources.tf
+++ Ben/terraform/resources.tf
@@ -2,3 +2,15 @@
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/storage_account
 # https://www.terraform.io/docs/language/data-sources/index.html
 
+resource "random_integer" "suffix" {
+  min = 1
+  max = 99999
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "ben${random_integer.suffix.result}"
+  resource_group_name      = data.azurerm_resource_group.padawans.name
+  location                 = data.azurerm_resource_group.padawans.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
