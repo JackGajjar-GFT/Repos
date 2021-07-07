11e5b1b379d5282da9d20876f3a1f48e77f228a2
Pasang <pasangsolti@hotmail.co.uk>
2021-07-07 11:23:27 +0100

    copy sample files

---
+++ Pasang/terraform/provider.tf
@@ -0,0 +1,14 @@
+# Terraform base config
+terraform {
+  required_providers {
+    google = {}
+  }
+
+  backend "gcs" {}
+
+}
+
+# Configure the Google Cloud Provider
+provider "google" {
+  project = "terraform-training-318910"
+}
